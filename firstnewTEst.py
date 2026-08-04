f5095fe3aeeca7602fb839a22f6d34a3943b56dc
DWgg <diwsgen@gmail.com>
2026-08-04 16:26:54 -0600

    Adding new instructions to the file

--- firstnewTEst.py
+++ firstnewTEst.py
@@ -29,7 +29,7 @@ so the steps will be:
 1. Create a new repository on github
 2. Copy the url of the repository
 3. Initialize a new git repository using git init
-4. Add files to the repository using git add <file_name>
+4. Add files to the repository using git add <file_name> / if we wanted to add all files we can use git add .
 5. Check the status of the repository using git status
 6. Commit the changes using git commit -m "commit message"
 7. Set the branch name using git branch -M main, this is optional since the default branch name is main, but this will be useful if we want to change the branch name to something else
@@ -38,5 +38,13 @@ so the steps will be:
 
 With that we have successfully created a new project and pushed it to github
 
+'''
+
+'''
+WE have made some changes to the project and we want to push those changes to the remote repository, we can do that using the following steps:
+1. Check the status of the repository using git status
+2. Add the changes to the repository using git add <file_name>
+3. Commit the changes using git commit -m "commit message"
+4. Push the changes to the remote repository using git push
 
 '''
